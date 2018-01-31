8ddea461b52c48c4ce83edcd57b16915d444968c
DanielSlater <daniel.slater@skimlinks.com>
2018-01-31 11:44:59 +0000

    fixed doc strings

--- pcams/content_addressable_memory.py
+++ pcams/content_addressable_memory.py
@@ -13,24 +13,45 @@ class EncoderInterface(object):
     def encode_batch(self, items):
         raise NotImplementedError()
 
+    @property
+    def encoded_dimensions(self):
+        raise NotImplementedError()
+
 
 class IdentityEncoder(EncoderInterface):
+    def __init__(self, encoded_dimensions):
+        """
+
+        Args:
+            encoded_dimensions (int):
+        """
+        assert encoded_dimensions > 0
+        self._encoded_dimensions = encoded_dimensions
+
     def encode_batch(self, items):
         return items
 
     def train(self, items):
         pass
 
+    @property
+    def encoded_dimensions(self):
+        return self._encoded_dimensions
+
 
 class ContentAddressableMemory(object):
-    def __init__(self, encoded_dimensions, encoder, distance_metric=mean_squared_error):
-        assert encoded_dimensions > 0
+    def __init__(self, encoder, distance_metric=mean_squared_error):
+        """
+
+        Args:
+            encoder (EncoderInterface):
+            distance_metric:
+        """
         assert isinstance(encoder, EncoderInterface)
 
-        self._dimensions = encoded_dimensions
         self._encoder = encoder
         self._distance_metric = distance_metric
-        self._root = BTreeNode.create_root(encoded_dimensions, DEFAULT_ITEMS_PER_LEAF)
+        self._root = BTreeNode.create_root(encoder.encoded_dimensions, DEFAULT_ITEMS_PER_LEAF)
 
     def train(self, items, include_existing=True):
         if include_existing and any(self._root.get_population()):
@@ -47,12 +68,12 @@ class ContentAddressableMemory(object):
 
     def get_similar(self, item, size):
         encoding, item = self._encoder.encode_batch([item])[0]
-        return self._root.get_similar(encoding, size=size+1, distance_func=self._distance_metric)
+        return self._root.get_similar(encoding, size=size + 1, distance_func=self._distance_metric)
 
     def rebuild(self):
         all_items = list(item for _, item in self._root.get_population())
 
         # TODO maybe on rebuild we can be smarter about leaf size? (and maybe dimensions even?)
-        self._root = BTreeNode.create_root(self._dimensions, DEFAULT_ITEMS_PER_LEAF)
+        self._root = BTreeNode.create_root(self._encoder.encoded_dimensions, DEFAULT_ITEMS_PER_LEAF)
         self._encoder.train(all_items)
         self.add_batch(all_items)
